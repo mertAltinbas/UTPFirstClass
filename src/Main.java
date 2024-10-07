8187d929000565b113bd867656772d4068e564ed
buyucukedi <scccgdh@gmail.com>
2024-10-07 11:30:18 +0200

    Cem is adding the Multi function and added Scanner fucntion to take variables from user

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,23 @@
 // OK, I will add `Substractor` and Cem will add `Adder`.
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Please enter 2 variables.");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(a, b));
 
         Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
+        System.out.println(subtractor.subtract(a, b));
+        Multi multi = new Multi();
+        System.out.println(multi.multi(a, b));
+
     }
+
     //sex
 }
---
+++ src/Multi.java
@@ -0,0 +1,5 @@
+public class Multi {
+    public int multi(int a, int b) {
+        return a * b;
+    }
+}
